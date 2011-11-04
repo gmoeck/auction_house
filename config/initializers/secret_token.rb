654bcbb25f01a711ad6f4e85aaebb28c1e56f818
Gregory Moeck <gmoeck@gmail.com>
2011-11-03 18:34:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuctionHouse::Application.config.secret_token = 'c687e6ade81b2a5423a529cff72f3ac7c6ca2ecce5f655085c4dfba05b4812a69e6144aa51b602ed52c8d446d47f799d89f2eb0eefad2bba01a1755e7cbedf0e'
