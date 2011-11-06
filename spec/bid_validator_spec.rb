baf118a9170a06d3d6cd4a5ca4668e9623e829a7
Gregory Moeck <gmoeck@gmail.com>
2011-11-05 17:29:43 -0700

    reject bid for negative values

--- spec/bid_validator_spec.rb
+++ spec/bid_validator_spec.rb
@@ -14,10 +14,17 @@ describe AuctionHouse::BidValidator do
     @bid_validator.bid("Joe", 15.00)
   end
 
-  it "reports the bid was not successful when a bid arrives that is less than the current max bid" do
+  it "reports the bid failed when a bid arrives that is less than the current max bid" do
     @bid_listener.should_receive(:bid_failed)
 
     @bid_validator.highest_bid_updated(AuctionHouse::Bid.new("Joe", 15.00))
     @bid_validator.bid("Jane", 14.50)
+  end
+
+  it "reports a failed bid when the value is negative" do
+    @bid_listener.should_receive(:bid_failed)
+    @bid_processor.should_not_receive(:bid)
+
+    @bid_validator.bid("Joe", -5.50)
   end
 end
